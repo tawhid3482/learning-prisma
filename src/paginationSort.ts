760df71dd9d130d7fad2ca139447b51d2a388dbd
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-04-29 15:59:45 +0600

    pagination sort

--- src/paginationSort.ts
+++ src/paginationSort.ts
@@ -3,7 +3,30 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 const pagination = async () => {
-  const findallData = await prisma.post.findMany();
-  console.log(findallData);
+  // offset pagination
+  //   const findallData = await prisma.post.findMany({
+  //     skip: 5,
+  //     take: 2,
+  //   });
+  //   console.log(findallData);
+
+  // cursor pagination
+  //   const cursorBaseallData = await prisma.post.findMany({
+  //     skip: 5,
+  //     take: 2,
+  //     cursor: {
+  //       id: 15,
+  //     },
+  //   });
+  //   console.log(cursorBaseallData);
+  const sortData = await prisma.post.findMany({
+    orderBy: {
+      id: "asc",
+    },
+    where: {
+      title: "this is title",
+    },
+  });
+  console.log(sortData);
 };
 pagination();
